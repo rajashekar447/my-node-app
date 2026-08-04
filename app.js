d03c9ac2c0cf3e3100c454dd34ace1b06b93d33a
Ubuntu <ubuntu@ip-172-31-47-87.ap-south-1.compute.internal>
2026-08-04 13:15:39 +0000

    Test GitHub webhook automation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
-    res.send("Hello Jenkins Pipeline!");
+    res.send("Hello Jenkins-nexus Pipeline!");
 });
 
 const PORT = process.env.PORT || 3000;
